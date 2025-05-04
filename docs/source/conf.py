c15a5b93d0b22f2420a0e607467add8cbc65f531
littl3gh0st <71267811+littl3gh0st@users.noreply.github.com>
2025-05-05 01:22:19 +0630

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'project_KB'
+copyright = '2025 KB_project'
+author = 'wai.lo'
 
 release = '0.1'
 version = '0.1.0'
