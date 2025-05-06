3176c567b9f8881e766dd320e96b12daaef926ea
littl3gh0st <71267811+littl3gh0st@users.noreply.github.com>
2025-05-06 10:42:46 +0630

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ templates_path = ['_templates']
 html_theme = 'sphinx_rtd_theme'
 
 def setup(app):
-    app.add_stylesheet('css/test.css')
+    app.add_css_file('test.css')
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
