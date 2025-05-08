6172d43a48efe0d5d11c8e3121682f367464f19b
littl3gh0st <71267811+littl3gh0st@users.noreply.github.com>
2025-05-08 16:52:03 +0630

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 html_search_language = 'en'
+html_search_options = {'stemming': False}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
